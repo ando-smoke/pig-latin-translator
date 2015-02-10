c4befe01f65ea6b4997082b178b90eb16191a79b
Andy Uppendahl <a.uppendahl@gmail.com>
2015-02-09 16:19:26 -0800

    Allow pigLatin function to correctly return words originally beginning with 1+ consonants ('qu' sequence included).

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var pigLatin = function(word) {
-  var vowelsRegEx = /^[aeiou]/;
+  var vowelsRegEx = /^[aeiou]/i;
+  var consonantsRegEx = /^([bcdfghjklmnpqrstvwxyz]+u*)(\w+)/i;
   var pigLatinWord = "";
 
   if (vowelsRegEx.test(word)) {
     pigLatinWord = word + "ay";
+  }
+  else if (consonantsRegEx.test(word)) {
+    pigLatinWord = word.replace(consonantsRegEx, "$2$1ay");
   }
   return pigLatinWord;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,20 @@ describe('pigLatin', function() {
   it('takes a word starting with a vowel and returns the word with "ay" at the end', function() {
     expect(pigLatin("inchworm")).to.equal("inchwormay");
   });
+
+  it('takes a word starting with 1+ consonants and returns the word that has all of those first consecutive consonants moved to the end and add "ay"', function() {
+    expect(pigLatin("schwartz")).to.equal("artzschway");
+  });
+
+  it('takes a word starting with 1+ consonants and returns the word that has all of those first consecutive consonants moved to the end and add "ay"', function() {
+    expect(pigLatin("trying")).to.equal("ingtryay");
+  });
+
+  it('takes a word starting with 1+ consonants ("qu" included) and returns the word that has all of those first consecutive consonants moved to the end and add "ay"', function() {
+    expect(pigLatin("question")).to.equal("estionquay");
+  });
+
+  it('takes a word starting with 1+ consonants ("qu" included) and returns the word that has all of those first consecutive consonants moved to the end and add "ay"', function() {
+    expect(pigLatin("squeal")).to.equal("ealsquay");
+  });
 });
